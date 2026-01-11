23182b3ca4782f47ce32fe429a3ec357d8fecfce
etnevelpriv <leventerastovits@gmail.com>
2026-01-11 19:21:18 +0100

    Kitoroltem mindet. Egyet benne hagyott logikusan, majd utolag kiszedjuk.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,42 @@
 const init = async function () {
-    databaseReset();
+    const URL = 'https://retoolapi.dev/cFJq9K/petrikBudi'
+    fetchGET(URL);
 };
 
-const databaseReset = async function () {
-  try {
-    const response = await fetch('https://retoolapi.dev/cFJq9K/petrikBudi', {
-        method: 'GET',
-    });
-    if (!response.ok) {
-        throw new Error(`Hibakod: ${response.status}. Hibauzenet: ${response.statusText}. Hibas URL: ${response.url}. Teljes hibauzenet: ${await response.text()}`);
+const fetchGET = async function (url) {
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+        });
+        if (!response.ok) {
+            throw new Error(`Hibakod: ${response.status}. Hibauzenet: ${response.statusText}. Hibas URL: ${response.url}. Teljes hibauzenet: ${await response.text()}`);
+        };
+        const databaseArray = await response.json()
+        // fetchDELETE(url, databaseArray)
+    } catch (err) {
+        throw new Error(err);
     };
-    console.log(await response.text());
-  } catch (err) {
-    throw new Error(err);
-  };
+};
+
+const fetchDELETE = async function (url, arr) {
+
+    for(const element of arr) {
+        const id = element.id;
+        try {
+            const response = await fetch(`${url}/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+            });
+            if (!response.ok) {
+                throw new Error(`Hibakod: ${response.status}. Hibauzenet: ${response.statusText}. Hibas URL: ${response.url}. Teljes hibauzenet: ${await response.text()}`);
+            };
+            console.log(await response.text())
+        } catch (err) {
+            throw new Error(err);
+        };
+    };
 };
 
 document.addEventListener('DOMContentLoaded', init)
